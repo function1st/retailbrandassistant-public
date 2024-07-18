e94450f499fe77ebf5c8697fdab84d1b17dfe535
Fn First <129132283+function1st@users.noreply.github.com>
2024-07-18 15:40:59 -0700

    Update start.js
    simplifying startup

--- reactfrontend/scripts/start.js
+++ reactfrontend/scripts/start.js
@@ -1,25 +1,15 @@
-const readline = require('readline');
 const { spawn } = require('child_process');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const hideSystemMessages = true;
+console.log(`System messages will be ${hideSystemMessages ? 'hidden' : 'shown'}`);
+
+const env = { ...process.env, REACT_APP_HIDE_SYSTEM_MESSAGES: hideSystemMessages };
+
+const child = spawn('react-scripts', ['start'], { 
+  stdio: 'inherit',
+  env: env
 });
 
-rl.question('Turn Off System Messages in the UX? (y/N) ', (answer) => {
-  const hideSystemMessages = answer.toLowerCase() === 'y';
-  console.log(`System messages will be ${hideSystemMessages ? 'hidden' : 'shown'}`);
-  
-  const env = { ...process.env, REACT_APP_HIDE_SYSTEM_MESSAGES: hideSystemMessages };
-  
-  const child = spawn('react-scripts', ['start'], { 
-    stdio: 'inherit',
-    env: env
-  });
-
-  child.on('exit', (code) => {
-    process.exit(code);
-  });
-
-  rl.close();
+child.on('exit', (code) => {
+  process.exit(code);
 });
